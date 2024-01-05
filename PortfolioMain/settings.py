dea7a7fabe82fb73ff2a2afb58631767c8ddc7e5
shujan-ahamed <ahamedshujan00@gmail.com>
2024-01-05 21:49:48 +0600

    renew 2024

--- PortfolioMain/settings.py
+++ PortfolioMain/settings.py
@@ -87,6 +87,7 @@ WSGI_APPLICATION = 'PortfolioMain.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
